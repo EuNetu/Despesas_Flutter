ceea5fb4fd72cffc8a06d165cfd8c18b51b7ad39
Euclides Neto <euneto2501@gmail.com>
2022-05-29 20:21:13 -0300

    create widget

---
+++ expenses/lib/components/transaction_user.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'transaction_Form.dart';
+import 'transaction_List.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser>{
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Monitor samsung',
+        value: 1200.00,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2', 
+      title: 'sucão', 
+      value: 35.00, 
+      date: DateTime.now()),
+  ];
+
+  _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        transactionList(_transactions),
+        TransactionForm(_addTransaction),
+      ],
+    );
+  }
+
+}
--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:expenses/models/transaction.dart';
+import 'components/transaction_user.dart';
+import '/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpenseApp());
@@ -13,18 +14,6 @@ class ExpenseApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final _transactions = [
-    Transaction(
-        id: 't1',
-        title: 'Monitor samsung',
-        value: 1200.00,
-        date: DateTime.now()),
-    Transaction(
-      id: 't2', 
-      title: 'sucão', 
-      value: 35.00, 
-      date: DateTime.now()),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +22,6 @@ class MyHomePage extends StatelessWidget {
           title: Text("Despesas Pessoais"),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
@@ -42,56 +30,7 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          tr.value.toString(),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16
-
-                            ),
-                          ),
-                          Text(
-                            tr.date.toString(),
-                            style: TextStyle(
-                              color: Colors.grey
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                );
-              }).toList(),
-            ),
+            TransactionUser(),
           ],
         ));
   }
