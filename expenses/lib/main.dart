077480b0396fb9fa1b7832b9468679e4bce5d6cd
Euclides Neto <euneto2501@gmail.com>
2022-05-28 22:14:37 -0300

    initial structure

---
+++ expenses/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpenseApp());
+
+class ExpenseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text("gráfico"),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text("Lista de despesas"),
+            ),
+          ],
+        ));
+  }
+}
