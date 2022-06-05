4fc4b8c29fb4abbe9706f67b471da98fe33a3b6e
Euclides Neto <euneto2501@gmail.com>
2022-06-05 20:58:05 -0300

    updated value graphic

--- expenses/lib/components/transaction_List.dart
+++ expenses/lib/components/transaction_List.dart
@@ -46,7 +46,7 @@ class TransactionList extends StatelessWidget {
                       child: Padding(
                         padding: const EdgeInsets.all(6),
                         child: FittedBox(
-                          child: Text('R\$${tr.value}'),
+                          child: Text('${tr.value}'),
                         ),
                       ),
                     ),
